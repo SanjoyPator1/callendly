cfa85a604cfaac80d95cf6fb257dcbbf90115db5
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-10 10:39:10 +0530

    first version completed for roll out - code part

--- packages/callendly/src/customCalendar/calendar.tsx
+++ packages/callendly/src/customCalendar/calendar.tsx
@@ -23,17 +23,21 @@ import {
 import { useState } from "react";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 import clsx from "clsx";
-import { CalendarClassModel, CalendarStyleModel } from "../../models/callendlyModel";
+import {
+  CalendarClassModel,
+  CalendarStyleModel,
+} from "../../models/callendlyModel";
 
 interface CalendarProps extends CalendarClassModel, CalendarStyleModel {
   currentMonthProp?: string;
   setCurrentMonthProp?: React.Dispatch<React.SetStateAction<string>>;
   setDaysProp?: React.Dispatch<React.SetStateAction<Date[]>>;
   onDateClick?: (date: Date) => void | Date;
+  onPrevMonthClick?: () => void | Date;
+  onNextMonthClick?: () => void | Date;
   eventDataProps?: any[];
   eventKeyName?: string;
-  weekDaysType?: "small" | "medium"
-  
+  weekDaysType?: "small" | "medium";
 }
 
 const CustomCalendar: FC<CalendarProps> = ({
@@ -41,6 +45,8 @@ const CustomCalendar: FC<CalendarProps> = ({
   setCurrentMonthProp,
   setDaysProp,
   onDateClick,
+  onPrevMonthClick,
+  onNextMonthClick,
   eventDataProps,
   eventKeyName,
   weekDaysType,
@@ -87,9 +93,10 @@ const CustomCalendar: FC<CalendarProps> = ({
   eventBubbleContainerStylesProp,
   eventBubbleStylesProp,
 }) => {
-
   let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  weekDaysType && weekDaysType==="small" && (daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"])
+  weekDaysType &&
+    weekDaysType === "small" &&
+    (daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"]);
 
   let today = startOfToday();
   let [selectedDay, setSelectedDay] = useState(today);
@@ -123,11 +130,13 @@ const CustomCalendar: FC<CalendarProps> = ({
   function previousMonth() {
     let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
     setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+    onPrevMonthClick && onPrevMonthClick();
   }
 
   function nextMonth() {
     let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
     setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+    onNextMonthClick && onNextMonthClick();
   }
 
   const handleOnDateClick = (day: Date) => {
@@ -136,60 +145,138 @@ const CustomCalendar: FC<CalendarProps> = ({
   };
 
   return (
-    <div className={clsx(styles.calendarContainer)}>
+    <div
+      className={clsx(styles.calendarContainer, calendarContainerClassProp)}
+      style={Object.assign({}, calendarContainerStylesProp)}
+    >
       {/* Header - current date and prev next button */}
-      <div className={clsx(styles.header)}>
-        <div className={clsx(styles.headerDate)}>
-          <p className={clsx(styles.headerDateFont)}>
+      <div
+        className={clsx(styles.header, headerClassProp)}
+        style={Object.assign({}, headerStylesProp)}
+      >
+        <div className={clsx(styles.headerDate, headerDateClassProp)}
+          style={Object.assign({}, headerDateStylesProp)}
+        >
+          <p className={clsx(styles.headerDateFont, headerDateFontClassProp)}
+            style={Object.assign({}, headerDateFontStylesProp)}
+          >
             {format(firstDayCurrentMonth, "MMMM yyyy")}
           </p>
         </div>
-        <div className={clsx(styles.headerButtonsContainer)}>
-          <button className={clsx(styles.prevMonthButton)} onClick={previousMonth}>
+        <div
+          className={clsx(
+            styles.headerButtonsContainer,
+            headerButtonsContainerClassProp
+          )}
+          style={Object.assign({}, headerButtonsContainerStylesProp)}
+        >
+          <button
+            className={clsx(styles.prevMonthButton, prevMonthButtonClassProp)}
+            style={Object.assign({}, prevMonthButtonStylesProp)}
+            onClick={previousMonth}
+          >
             <BiLeftArrowAlt />
           </button>
-          <button className={clsx(styles.nextMonthButton)} onClick={nextMonth}>
+          <button
+            className={clsx(styles.nextMonthButton, nextMonthButtonClassProp)}
+            style={Object.assign({}, nextMonthButtonStylesProp)}
+            onClick={nextMonth}
+          >
             <BiRightArrowAlt />
           </button>
         </div>
       </div>
 
       {/* Render days of week */}
-      <div className={clsx(styles.sevenGrid, styles.daysOfWeekContainer)}>
+      <div
+        className={clsx(
+          styles.sevenGrid,
+          styles.daysOfWeekContainer,
+          sevenGridClassProp,
+          daysOfWeekContainerClassProp
+        )}
+        style={Object.assign({}, sevenGridStylesProp, daysOfWeekContainerStylesProp)}
+      >
         {daysOfWeek.map((day) => (
-          <div className={clsx(styles.weekDaysKeyContainer)} key={day}>
-            <p className={clsx(styles.weekDaysFont)}>{day}</p>
+          <div
+            className={clsx(
+              styles.weekDaysKeyContainer,
+              weekDaysKeyContainerClassProp
+            )}
+            style={Object.assign({}, weekDaysKeyContainerStylesProp)}
+            key={day}
+          >
+            <p className={clsx(styles.weekDaysFont, weekDaysFontClassProp)}
+              style={Object.assign({}, weekDaysFontStylesProp)}
+            >
+              {day}
+            </p>
           </div>
         ))}
       </div>
 
       {/* Render days of month */}
-      <div className={clsx(styles.sevenGrid, styles.daysOfMonth)}>
+      <div
+        className={clsx(
+          styles.sevenGrid,
+          styles.daysOfMonth,
+          sevenGridClassProp,
+          daysOfMonthClassProp
+        )}
+        style={Object.assign({}, sevenGridStylesProp, daysOfMonthStylesProp)}
+      >
         {days &&
           days.map((day, index) => {
             return (
-              <div className={clsx(styles.monthDaysKeyContainer)} key={index}>
+              <div
+                className={clsx(
+                  styles.monthDaysKeyContainer,
+                  monthDaysKeyContainerClassProp
+                )}
+                style={Object.assign({}, monthDaysKeyContainerStylesProp)}
+                key={index}
+              >
                 <button
                   className={clsx(
                     styles.button,
-                    isToday(day) && styles.isToday,
                     isSameMonth(day, firstDayCurrentMonth) &&
                       styles.isSameMonth,
-                    isEqual(day, selectedDay) && styles.isEqual
+                    isEqual(day, selectedDay) && styles.isEqual,
+                    isToday(day) && styles.isToday,
+                    buttonClassProp,
+                    isSameMonthClassProp,
+                    isEqualClassProp,
+                    isTodayClassProp,
                   )}
                   onClick={() => handleOnDateClick(day)}
-                  style={{ gridColumnStart: getDay(day) + 1 }}
+                  style={Object.assign({},{
+                    gridColumnStart: getDay(day) + 1,
+                  },buttonStylesProp,isSameMonthStylesProp,isEqualStylesProp,isTodayStylesProp )}
                 >
                   <time dateTime={format(day, "yyyy-MM-dd")}>
                     {format(day, "d")}
                   </time>
                 </button>
-                <div className={clsx(styles.eventBubbleContainer)}>
+                <div
+                  className={clsx(
+                    styles.eventBubbleContainer,
+                    eventBubbleContainerClassProp
+                  )}
+                  style={Object.assign({}, eventBubbleContainerStylesProp)}
+                >
                   {eventData &&
                     eventKeyName &&
                     eventData.some((event) => {
                       return isSameDay(event[eventKeyName], day);
-                    }) && <div className={clsx(styles.eventBubble)}></div>}
+                    }) && (
+                      <div
+                        className={clsx(
+                          styles.eventBubble,
+                          eventBubbleClassProp
+                        )}
+                        style={Object.assign({}, eventBubbleStylesProp)}
+                      ></div>
+                    )}
                 </div>
               </div>
             );
