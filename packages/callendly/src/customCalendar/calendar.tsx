b2169949267645c674ba3fab3e0e450f803a562e
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-12 17:18:38 +0530

    js docs added

--- packages/callendly/src/customCalendar/calendar.tsx
+++ packages/callendly/src/customCalendar/calendar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC,useEffect } from "react";
+import React, { FC, useEffect } from "react";
 import styles from "./calendar.module.css";
 import "./mystyle.css";
 
@@ -19,7 +19,7 @@ import {
   startOfWeek,
 } from "date-fns";
 import { useState } from "react";
-import {LeftArrowIcon,RightArrowIcon} from "../customIcons/CustomIcons"
+import { LeftArrowIcon, RightArrowIcon } from "../customIcons/CustomIcons";
 import clsx from "clsx";
 import {
   CalendarClassModel,
@@ -27,6 +27,7 @@ import {
 } from "../../models/callendlyModel";
 
 interface CalendarProps extends CalendarClassModel, CalendarStyleModel {
+  viewPrevNextMonth?: boolean;
   setDaysProp?: React.Dispatch<React.SetStateAction<Date[]>>;
   onDateClick?: (date: Date) => void | Date;
   onDateMouseEnter?: (date: Date) => void | Date;
@@ -38,8 +39,143 @@ interface CalendarProps extends CalendarClassModel, CalendarStyleModel {
   eventDataProps?: any[];
   eventKeyName?: string;
   weekDaysType?: "small" | "medium";
-  viewPrevNextMonth?: boolean
 }
+
+/**
+ * @function CustomCalendar
+ * @description A simple light weight fully customizable calendar component.
+ * @returns {JSX.Element} - A React element that displays the calendar.
+ * 
+ * @prop  {function} props.setDaysProp - A state setter function that updates the list of days to display.
+ * @prop  {function} props.onDateClick - A function that is called when a date is clicked.
+ * @prop  {function} props.onDateMouseEnter - A function that is called when the mouse enters a date.
+ * @prop  {function} props.onDateMouseLeave - A function that is called when the mouse leaves a date.
+ * @prop  {function} props.onPrevMonthClick - A function that is called when the "previous month" button is clicked.
+ * @prop  {function} props.onNextMonthClick - A function that is called when the "next month" button is clicked.
+ * @prop  {JSX.Element} [props.prevMonthButtonIcon] - An optional icon element to use for the "previous month" button.
+ * @prop  {JSX.Element} [props.nextMonthButtonIcon] - An optional icon element to use for the "next month" button.
+ * @prop  {Array<object>} [props.eventDataProps] - An optional array of event data objects.
+ * @prop  {string} [props.eventKeyName] - An optional key name to use when accessing event data properties.
+ * @prop  {string} [props.weekDaysType] - An optional string representing the type of week days to display.
+ * @prop  {boolean} [props.viewPrevNextMonth=true] - A boolean indicating whether the "previous month" and "next month" dates should be displayed along with the current date.
+ * 
+ * @example <caption>simple calendar display</caption>
+ * 
+    import { Calendar } from "callendly";
+    <Calendar />
+ * 
+ *
+ * @example <caption>view previous and next month days on the current calendar</caption>
+ * 
+    import { Calendar } from "callendly";
+    <Calendar  viewPrevNextMonth={true}/>
+ * 
+ * 
+ * @example <caption>Get the days data currently displaying</caption>
+ * 
+    import { Calendar } from "callendly";
+    import { useState } from "react";
+
+    export default function MyCalendar() {
+      const [days, setDays] = useState([]);
+    
+      return (
+        <div>
+          <Calendar setDaysProp={setDays} />
+          <pre>{JSON.stringify(days, null, 2)}</pre>
+        </div>
+      );
+    }
+
+ * 
+ *
+ * @example <caption>On Date Click</caption>
+ * 
+    import { Calendar } from "callendly";
+    const handleOnDateClick = (selectedDate : Date) =>{
+      console.log("selectedDate is ",selectedDate);
+    }
+    <Calendar  onDateClick={handleOnDateClick}/>
+ * 
+ *
+ * @example <caption>On Date mouse enter</caption>
+ * 
+    import { Calendar } from "callendly";
+    const handleOnDateMouseEnter = (dateMouseEnter : Date) =>{
+      console.log("Date mouse enter is ",dateMouseEnter);
+    }
+    <Calendar  onDateMouseEnter={handleOnDateMouseEnter}/>
+ * 
+ *
+ * @example <caption>On Date mouse leave</caption>
+ * 
+    import { Calendar } from "callendly";
+    const handleOnDateMouseLeave = (dateMouseLeave : Date) =>{
+      console.log("Date mouse leave is ",dateMouseLeave);
+    }
+    <Calendar  onDateMouseLeave={handleOnDateMouseLeave}/>
+ * 
+ *
+ * @example <caption>On previous month button click </caption>
+ * 
+    import { Calendar } from "callendly";
+    const handleOnPrevMonthClick = () =>{
+      console.log("previous month button is clicked");
+    }
+    <Calendar  onPrevMonthClick={handleOnPrevMonthClick}/>
+ * 
+ *
+ * @example <caption>On next month button click </caption>
+ * 
+    import { Calendar } from "callendly";
+    const handleOnNextMonthClick = () =>{
+      console.log("next month button is clicked");
+    }
+    <Calendar  onNextMonthClick={handleOnNextMonthClick}/>
+ * 
+ *
+ * @example <caption> Event data is shown in calendar </caption>
+ * 
+    import { Calendar } from "callendly";
+    //event data needs to be an array of object with a key and value should be of type date
+    // The event example below has data on keyName meetingDate
+    const eventData = [
+      {
+        meetingDate: startOfDay(new Date()),
+        otherProperty: "some value",
+      },
+      {
+        meetingDate: startOfDay(addDays(new Date(), 1)),
+        otherProperty: "some value",
+      },
+      {
+        meetingDate: startOfDay(addDays(new Date(), 2)),
+        otherProperty: "some value",
+      },
+      {
+        meetingDate: startOfDay(addDays(new Date(), 3)),
+        otherProperty: "some value",
+      },
+      {
+        meetingDate: startOfDay(addDays(new Date(), 4)),
+        otherProperty: "some value",
+      },
+    ];
+    //pass the eventData and the eventKeyName on which your event is present. Here it is meetingDate
+    <Calendar eventDataProps={eventData} eventKeyName="meetingDate"/>
+ * 
+ * 
+ * @example <caption>Week days type</caption>
+ * 
+    import { Calendar } from "callendly";
+    <Calendar  weekDaysType={"small"}/>
+    //weekDaysType can be two values : "small" or "medium"
+    //Default is medium
+    //small = [S, M, T, W, T, F, S]
+    //medium = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
+ * 
+ * 
+ */
 
 const CustomCalendar: FC<CalendarProps> = ({
   setDaysProp,
@@ -54,7 +190,6 @@ const CustomCalendar: FC<CalendarProps> = ({
   eventKeyName,
   weekDaysType,
   viewPrevNextMonth = true,
-
 
   // class props
   calendarContainerClassProp,
@@ -110,16 +245,24 @@ const CustomCalendar: FC<CalendarProps> = ({
   const [eventData, setEventData] = useState(eventDataProps);
 
   let initialDaysData = eachDayOfInterval({
-    start: viewPrevNextMonth ? startOfWeek(firstDayCurrentMonth): firstDayCurrentMonth,
-    end: viewPrevNextMonth ? endOfWeek(endOfMonth(firstDayCurrentMonth)): endOfMonth(firstDayCurrentMonth),
+    start: viewPrevNextMonth
+      ? startOfWeek(firstDayCurrentMonth)
+      : firstDayCurrentMonth,
+    end: viewPrevNextMonth
+      ? endOfWeek(endOfMonth(firstDayCurrentMonth))
+      : endOfMonth(firstDayCurrentMonth),
   });
 
   const [days, setDays] = useState<Date[]>(initialDaysData);
 
   useEffect(() => {
     const daysData = eachDayOfInterval({
-      start: viewPrevNextMonth ? startOfWeek(firstDayCurrentMonth): firstDayCurrentMonth,
-      end: viewPrevNextMonth ? endOfWeek(endOfMonth(firstDayCurrentMonth)): endOfMonth(firstDayCurrentMonth),
+      start: viewPrevNextMonth
+        ? startOfWeek(firstDayCurrentMonth)
+        : firstDayCurrentMonth,
+      end: viewPrevNextMonth
+        ? endOfWeek(endOfMonth(firstDayCurrentMonth))
+        : endOfMonth(firstDayCurrentMonth),
     });
     setDays(daysData);
     setDaysProp && setDaysProp(daysData);
@@ -159,10 +302,12 @@ const CustomCalendar: FC<CalendarProps> = ({
         className={clsx(styles.header, headerClassProp)}
         style={Object.assign({}, headerStylesProp)}
       >
-        <div className={clsx(styles.headerDate, headerDateClassProp)}
+        <div
+          className={clsx(styles.headerDate, headerDateClassProp)}
           style={Object.assign({}, headerDateStylesProp)}
         >
-          <p className={clsx(styles.headerDateFont, headerDateFontClassProp)}
+          <p
+            className={clsx(styles.headerDateFont, headerDateFontClassProp)}
             style={Object.assign({}, headerDateFontStylesProp)}
           >
             {format(firstDayCurrentMonth, "MMMM yyyy")}
@@ -200,7 +345,11 @@ const CustomCalendar: FC<CalendarProps> = ({
           sevenGridClassProp,
           daysOfWeekContainerClassProp
         )}
-        style={Object.assign({}, sevenGridStylesProp, daysOfWeekContainerStylesProp)}
+        style={Object.assign(
+          {},
+          sevenGridStylesProp,
+          daysOfWeekContainerStylesProp
+        )}
       >
         {daysOfWeek.map((day) => (
           <div
@@ -211,7 +360,8 @@ const CustomCalendar: FC<CalendarProps> = ({
             style={Object.assign({}, weekDaysKeyContainerStylesProp)}
             key={day}
           >
-            <p className={clsx(styles.weekDaysFont, weekDaysFontClassProp)}
+            <p
+              className={clsx(styles.weekDaysFont, weekDaysFontClassProp)}
               style={Object.assign({}, weekDaysFontStylesProp)}
             >
               {day}
@@ -238,7 +388,11 @@ const CustomCalendar: FC<CalendarProps> = ({
                   styles.monthDaysKeyContainer,
                   monthDaysKeyContainerClassProp
                 )}
-                style={Object.assign({},{gridColumnStart: getDay(day) + 1}, monthDaysKeyContainerStylesProp)}
+                style={Object.assign(
+                  {},
+                  { gridColumnStart: getDay(day) + 1 },
+                  monthDaysKeyContainerStylesProp
+                )}
                 key={index}
               >
                 <button
@@ -251,13 +405,18 @@ const CustomCalendar: FC<CalendarProps> = ({
                     buttonClassProp,
                     isSameMonthClassProp,
                     isEqualClassProp,
-                    isTodayClassProp,
+                    isTodayClassProp
                   )}
                   onClick={() => handleOnDateClick(day)}
-                  onMouseEnter={()=> onDateMouseEnter && onDateMouseEnter(day)}
-                  onMouseLeave={()=> onDateMouseLeave && onDateMouseLeave(day)}
-                  style={Object.assign({},
-                    buttonStylesProp,isSameMonthStylesProp,isEqualStylesProp,isTodayStylesProp )}
+                  onMouseEnter={() => onDateMouseEnter && onDateMouseEnter(day)}
+                  onMouseLeave={() => onDateMouseLeave && onDateMouseLeave(day)}
+                  style={Object.assign(
+                    {},
+                    buttonStylesProp,
+                    isSameMonthStylesProp,
+                    isEqualStylesProp,
+                    isTodayStylesProp
+                  )}
                 >
                   <time dateTime={format(day, "yyyy-MM-dd")}>
                     {format(day, "d")}
